64294ef7f90eb7c665048d9e4c3a254e110e0e05
Zhell <zhellqol@gmail.com>
2024-02-08 16:58:50 +0100

    Fixes for non-legacy transfer systems

--- scripts/triggers/effect.mjs
+++ scripts/triggers/effect.mjs
@@ -2,17 +2,23 @@ import {MODULE} from "../constants.mjs";
 import {EffectMethods} from "../effectMethods.mjs";
 
 export class EffectTriggers {
+  /**
+   * Is legacy transfer on?
+   * @type {boolean}
+   */
+  static get isLegacy() {
+    return CONFIG.ActiveEffect.legacyTransferral;
+  }
+
   /* Initialize module. */
   static init() {
     Hooks.on("createActiveEffect", EffectTriggers.onCreateDelete.bind("onCreate"));
     Hooks.on("deleteActiveEffect", EffectTriggers.onCreateDelete.bind("onDelete"));
     Hooks.on("preUpdateActiveEffect", EffectTriggers.preUpdate);
     Hooks.on("updateActiveEffect", EffectTriggers.onUpdate);
-    // The below hooks are irrelevant to systems that do not use legacy transfer.
-    if (CONFIG.ActiveEffect.legacyTransferral) {
-      Hooks.on("preUpdateItem", EffectTriggers.preUpdateItem);
-      Hooks.on("updateItem", EffectTriggers.updateItem);
-    }
+    Hooks.on("preUpdateItem", EffectTriggers.preUpdateItem);
+    Hooks.on("updateItem", EffectTriggers.updateItem);
+    Hooks.on("deleteItem", EffectTriggers.deleteItem);
   }
 
   /**
@@ -22,8 +28,7 @@ export class EffectTriggers {
    * @param {object} context          The update options.
    */
   static async onUpdate(effect, update, context) {
-    const legacy = CONFIG.ActiveEffect.legacyTransferral;
-    if (legacy && (effect.parent instanceof Item)) return;
+    if (EffectTriggers.isLegacy && (effect.parent instanceof Item)) return;
 
     const run = EffectMethods.isExecutor(effect.parent);
     if (!run) return false;
@@ -47,8 +52,7 @@ export class EffectTriggers {
    * @param {object} context          The update options.
    */
   static preUpdate(effect, update, context) {
-    const legacy = CONFIG.ActiveEffect.legacyTransferral;
-    if (legacy && (effect.parent instanceof Item)) return;
+    if (EffectTriggers.isLegacy && (effect.parent instanceof Item)) return;
     const path = `${MODULE}.${effect.id}.wasOn`;
     foundry.utils.setProperty(context, path, effect.modifiesActor);
   }
@@ -70,23 +74,26 @@ export class EffectTriggers {
    * @param {object} context      The update options.
    */
   static preUpdateItem(item, update, context) {
-    const effects = item.actor?.effects.filter(e => e.origin === item.uuid) ?? [];
-    effects.forEach(e => foundry.utils.setProperty(context, `${MODULE}.${e.id}.wasOn`, e.modifiesActor));
+    if (!item.isEmbedded) return;
+    const collection = EffectTriggers.isLegacy ? item.actor.effects.filter(e => e.origin === item.uuid) : item.effects;
+    collection.forEach(e => foundry.utils.setProperty(context, `${MODULE}.${e.id}.wasOn`, e.modifiesActor));
   }
 
   /**
-   * Execute effect toggles if an item update results in an effect applying or not.
+   * Execute effect toggles if an item update results in an effect changing whether it affects an actor.
    * @param {Item} item           The item updated.
    * @param {object} update       The update performed.
    * @param {object} context      The update options.
    */
   static async updateItem(item, update, context) {
+    if (!item.isEmbedded) return;
     const run = EffectMethods.isExecutor(item.actor);
     if (!run) return;
 
     const ids = Object.keys(context[MODULE] ?? {});
     if (!ids.length) return;
-    const effects = ids.map(id => item.actor.effects.get(id));
+    const collection = EffectTriggers.isLegacy ? item.actor.effects : item.effects;
+    const effects = ids.map(id => collection.get(id));
 
     for (const effect of effects) {
       const isOn = effect.modifiesActor;
@@ -99,5 +106,19 @@ export class EffectTriggers {
       if (toggledOn && effect.hasMacro("onEnable")) await effect.callMacro("onEnable");
       if (toggled && effect.hasMacro("onToggle")) await effect.callMacro("onToggle");
     }
+  }
+
+  /**
+   * Execute effect deletion triggers when the parent item is deleted. This only applies to non-legacy transfer systems.
+   * @param {Item} item     The item being deleted.
+   * @param {object} options      Update options.
+   */
+  static async deleteItem(item, options) {
+    if (!item.isEmbedded || EffectTriggers.isLegacy) return;
+    const run = EffectMethods.isExecutor(item.actor);
+    if (!run) return;
+
+    const effects = item.effects.filter(e => e.modifiesActor && e.hasMacro("onDelete"));
+    for (const effect of effects) await effect.callMacro("onDelete");
   }
 }
